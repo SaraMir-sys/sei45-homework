8c9409194690f2e1d9f3e611811113c1a401366f
SaraMir-sys <sara.zina.mir@gmail.com>
2021-06-24 10:36:58 +1000

    word

--- Sara Mir/week1/day3/js/exercise.js
+++ Sara Mir/week1/day3/js/exercise.js
@@ -48,10 +48,57 @@ const vowel = function(t){
 
 //     Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
 const sumArray = function(B,T){
-  for (let i=0; i < B.length; i++);{
+for (let i=0; i < B.length; i++){
    k=B[i]+T[i];
  }
 console.log(k)
 }
 
-console.log(sumArray([2,2],[1,3]))
+sumArray([2,2],[1,3])
+
+
+// Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+
+const  reverseString= function(arr){
+for (let i=arr.length; i >1; i--){
+   console.log(arr[i]);
+ }
+}
+
+reverseString["a", "b", "b", "c"]
+
+
+
+
+
+// Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+const findLongestWord = function(arr) {
+		k === 0;
+		for (let i = 0; i < arr.length); {
+			t= arr[i].length;
+			if (t > k) {
+				k === t;
+			}
+		}
+		Console.log(k)
+}
+  findLongestWord["brak","jamira"]
+
+
+
+// Write a function filterLongWords that takes an array of words and an number i and returns the array of words that are longer than i.
+const filterLongWords = function(arr,n) {
+		if (arr.length>n);{
+      Console.log(arr);
+    }else{
+      console.log("no is bigger");
+  }
+  }
+
+  filterLongWords([2,3,5],2)
+
+
+
+
+//Homework: The Word Guesser
+// Create two global arrays: one to hold the letters of the word (e.g. 'F', 'O', 'X'), and one to hold the current guessed letters (e.g. it would start with '_', '_', '_' and end with 'F', 'O', 'X').
